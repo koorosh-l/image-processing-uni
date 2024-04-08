5ba9ba2fe90dc682b05907265b3c6046b0bbb8ee
koorosh <koorosh.akl@protonmail.com>
2024-04-08 09:39:35 +0300

    project1 done?

--- project1/project.py
+++ project1/project.py
@@ -7,20 +7,20 @@ import tensorflow as tf
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
-df_data = pd.DataFrame(data)
+digits = datasets.load_digits()
+totalItems = len(digits.images)
+flattened_images = digits.images.reshape(totalItems, -1)
+df_data = pd.DataFrame(flattened_images)
 df_data = df_data.astype('float32')
+
 X_train, X_test, y_train, y_test = train_test_split(df_data, digits.target, test_size=0.2, shuffle=False)
-model = tf.keras.Sequential([
-tf.keras.layers.Flatten(input_shape=(totalItems, 1)),
-tf.keras.layers.Dense(128, activation='relu'),
-tf.keras.layers.Dense(10)])
+
+model = tf.keras.Sequential([tf.keras.layers.Dense(128, activation='relu', input_shape=(64,)),
+                             tf.keras.layers.Dense(10, activation='softmax')])
 
 model.compile(optimizer='adam',
-
-loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-
-metrics=['accuracy'])
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 model.fit(X_train, y_train, epochs=100)
-
 test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
