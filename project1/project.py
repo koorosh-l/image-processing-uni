b4639eec14c0b0fe7d13c3cc344193fa8d09bd03
evenum <jackherer202@protonmail.com>
2024-04-14 20:11:38 +0300

    please please PLEASE

--- project1/project.py
+++ project1/project.py
@@ -7,29 +7,24 @@ import tensorflow as tf
 from sklearn import datasets, metrics
 from sklearn.model_selection import train_test_split
 
-plt.matplotlib.use('TkAgg')
 digits = datasets.load_digits()
 images = digits.images
 features = []
-for img in images:
-    thresh = cv2.threshold(img, 3, 255, cv2.THRESH_BINARY)[1]
-    contours, _ = cv2.findContours(np.uint8(thresh), 0, 3)
-    blank_img = np.zeros_like(img)
-    M = 0
-    for cnt in contours:
-        M += cv2.HuMoments(cv2.moments(cnt)).flatten()
-    # redrawn = cv2.drawContours(blank_img, contours, -1, (255, 255, 255), thickness=cv2.FILLED).flatten()
-    # features.append(np.concatenate(redrawn, M))
-    features.append(M)
+for img in images:    
+
+    blur = cv2.GaussianBlur(img, (3,3), 0)
+    normalize = blur / 255
+    moments = cv2.moments(normalize)
+    hu_moments = cv2.HuMoments(moments).flatten()
+    features.append(hu_moments)
 
 data = np.array(features)
-
 df_data = pd.DataFrame(data)
 df_data = df_data.astype('float32')
-print("--------------------")
-print(len(features))
-print(len(digits.target))
-print("--------------------")
+#print("--------------------")
+#print(len(features))
+#print(len(digits.target))
+#print("--------------------")
 X_train, X_test, y_train, y_test = train_test_split(df_data, digits.target, test_size=0.2, shuffle=False)
 
 model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(df_data.shape[1],)),
