9e9b99d335a91fb32a5ce2650a4a6cc4cae70267
evenum <jackherer202@protonmail.com>
2024-04-14 12:19:13 +0300

    Added explicit feature extraction (moments)

--- project1/project.py
+++ project1/project.py
@@ -4,12 +4,16 @@ import cv2
 import numpy as np
 import collections
 import tensorflow as tf
-from sklearn import datasets
+from sklearn import datasets, metrics
 from sklearn.model_selection import train_test_split
 
 digits = datasets.load_digits()
-totalItems = len(digits.images)
+images = digits.images
+totalItems = len(images)
+for i in range(totalItems):
+    images[i] = cv2.threshold(images[i],10, 255, cv2.THRESH_BINARY)[1]
 flattened_images = digits.images.reshape(totalItems, -1)
+
 df_data = pd.DataFrame(flattened_images)
 df_data = df_data.astype('float32')
 
@@ -24,3 +28,4 @@ model.compile(optimizer='adam',
 
 model.fit(X_train, y_train, epochs=100)
 test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+print(test_acc, test_loss)
