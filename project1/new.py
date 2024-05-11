302fc2c2519fa9bd95039c47d1f5ce1279a3ecf1
koorosh <koorosh.akl@protonmail.com>
2024-05-11 19:59:37 +0300

    ;asdlkfja;sdlfkja;dslfkjad;flkj

--- project1/new.py
+++ project1/new.py
@@ -6,9 +6,11 @@ import collections
 import tensorflow as tf
 import os
 from tensorflow.keras import layers, models
+plt.matplotlib.use('TkAgg')
+#os.environ["IMG"] = '<path-repo>/img/project1/'
 prefix = os.getenv("IMG")
 #fruits data set can be found here
-#https://www.kaggle.com/datasets/afsananadia/fruits-images-dataset-object-detection
+#https://www.kaggle.com/datasets/afsananadia/fruits-images-dataset-object-detectino
 def list_map(proc, items):
     return list(map(proc, items))
 
@@ -19,12 +21,11 @@ def list_files_in_directory(directory):
     return files
 
 def find_bounding_rectangles(image):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
     contours, _ = cv2.findContours(gray_image,
                                    cv2.RETR_EXTERNAL,
                                    cv2.CHAIN_APPROX_SIMPLE)
     bounding_rectangles = []
-    print(contours)
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
         bounding_rectangles.append((x, y, w, h))
@@ -57,11 +58,17 @@ train_images = list_map(cv2.imread,
                         train_names)
 test_images  = list_map(cv2.imread,
                         test_names)
-# train_labels =
-# test_labels  =
+train_names = list_map(lambda str:
+                       os.path.basename(str).split('_')[1].split('.')[0].lower()
+                       ,train_names)
+test_names = list_map(lambda str:
+                       os.path.basename(str).split('_')[1].split('.')[0].lower()
+                       ,test_names)
+
 data = np.array(list_map(pad(find_bounding_rectangles(d)), train_images))
 #np.array(pad([(0, 0, 2827, 2827)])).flatten()
 model = create_model()
 model.compile(optimizer='adam',
               loss=tf.keras.losses.sparse_categorical_crossentropy,
               metrics='accuracy')
+model.
