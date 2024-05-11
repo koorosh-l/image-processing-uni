6979691f108ef3bcd0d8c07adc9904847edfe0a0
koorosh <koorosh.akl@protonmail.com>
2024-05-11 18:43:31 +0300

    list map added

--- project1/new.py
+++ project1/new.py
@@ -6,11 +6,11 @@ import collections
 import tensorflow as tf
 import os
 from tensorflow.keras import layers, models
-from sklearn import datasets, metrics
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 prefix = os.getenv("IMG")
+#fruits data set can be found here
 #https://www.kaggle.com/datasets/afsananadia/fruits-images-dataset-object-detection
+def list_map(proc, items):
+    return list(map(proc, items))
 
 def list_files_in_directory(directory):
     directory = directory
@@ -49,21 +49,17 @@ def create_model():
 #data prepreation
 test_path = "fruits/test/"
 train_path = "fruits/train/"
-train_names = list(map(lambda str: prefix + train_path + str,
-                  list_files_in_directory(prefix + train_path)))
-test_names  = list(map(lambda str: prefix + test_path + str,
-                  list_files_in_directory(prefix + test_path)))
-train_images = list(map(cv2.imread,
-                        train_names))
-test_images  = list(map(cv2.imread,
-                   test_names))
-train_labels =
-test_labels  =
-
-
-data = np.array(map((lambda d:
-                     np.array(pad(find_bounding_rectangles(d)))),
-                    data))
+train_names = list_map(lambda str: prefix + train_path + str,
+                       list_files_in_directory(prefix + train_path))
+test_names  = list_map(lambda str: prefix + test_path + str,
+                       list_files_in_directory(prefix + test_path))
+train_images = list_map(cv2.imread,
+                        train_names)
+test_images  = list_map(cv2.imread,
+                        test_names)
+# train_labels =
+# test_labels  =
+data = np.array(list_map(pad(find_bounding_rectangles(d)), train_images))
 #np.array(pad([(0, 0, 2827, 2827)])).flatten()
 model = create_model()
 model.compile(optimizer='adam',
