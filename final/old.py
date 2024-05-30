43b24caef005199cb27403ca4a91db5517dd8307
koorosh <koorosh.akl@protonmail.com>
2024-05-30 20:19:00 +0300

    start of the end

---
+++ final/old.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import cv2
+import numpy as np
+import collections
+import tensorflow as tf
+import os
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import layers, models
